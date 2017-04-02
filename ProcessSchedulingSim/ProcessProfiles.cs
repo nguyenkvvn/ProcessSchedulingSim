b50ff14f5c81a775d184bfe4a4e8791811ac39f7
Vinh Nguyen <nguyen.kvvn@gmail.com>
2017-04-02 00:52:19 -0400

    CSV Input Processing
    CSV files in the format of rows of "priority, submision time, CPU burst
    time" seperated by commas per column can be interpreted and parsed as
    input.
    
    A console will appear upon launch to request an absolute path to the
    file.

---
+++ ProcessSchedulingSim/ProcessProfiles.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//referring to StackOverflow tutorial
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ProcessSchedulingSim
+{
+    //This class is a wrapper for the input CSV, and will process the CSV into a list of values based on index. This is to simplify having redundant code read in and out from a CSV.
+    class ProcessProfiles
+    {
+        List<int> LPriority;
+        List<int> LSubmissionTime;
+        List<int> LCPUBurstTime;
+        List<ProcessProfile> listOfProfiles;
+
+        public ProcessProfiles(string pathToInput)
+        {
+            LPriority = new List<int>();
+            LSubmissionTime = new List<int>();
+            LCPUBurstTime = new List<int>();
+            listOfProfiles = new List<ProcessProfile>();
+            interpret(pathToInput);
+        }
+
+        //interpret will read into the CSV
+        private Boolean interpret(string path)
+        {
+            //referring to StackOverflow tutorial on reading CSV
+            using (TextFieldParser parser = new TextFieldParser(@path))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    //for each column
+                    int csvCounter = 0;
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    foreach (string field in fields)
+                    {
+                        switch (csvCounter)
+                        {
+                            //Priority Column
+                            case 0:
+                                csvCounter++;
+                                LPriority.Add(System.Convert.ToInt32(field));
+                                Console.Write("Row Parsed: " + field);
+                                break;
+                            //Submission Time Column
+                            case 1:
+                                csvCounter++;
+                                LSubmissionTime.Add(System.Convert.ToInt32(field));
+                                Console.Write("," + field);
+                                break;
+                            //CPU Burst Time Column
+                            case 2:
+                                csvCounter = 0;
+                                LCPUBurstTime.Add(System.Convert.ToInt32(field));
+                                Console.WriteLine("," + field);
+                                break;
+                        }
+                    }
+                }
+            }
+
+            //process the seperated values into one ProcessProfile object
+            for (int i = 0; i < LPriority.Count; i++)
+            {
+                ProcessProfile pp = new ProcessProfile(LPriority[i], LSubmissionTime[i], LCPUBurstTime[i]);
+
+                //drop it into the list of profiles
+                listOfProfiles.Add(pp);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ProcessSchedulingSim/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessSchedulingSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Process Scheduling Simulator");
+            Console.WriteLine("Kevin Vinh Nguyen");
+            Console.WriteLine("CSC 4320 - Operating Systems");
+            Console.WriteLine("Chad Frederick");
+            Console.WriteLine("--------------------");
+
+            //take in input CSV file
+            //format: priority, submission time, cpu burst time, IO burst time, CPU, IO?
+            //number of rows is number of processes (profiles)
+            Console.WriteLine("\nPlease enter path for CSV CPU profile sample:");
+            string path = Console.ReadLine();
+
+            ProcessProfiles pp = new ProcessProfiles(path);
+
+            //simulate running the following scheduling algorithms
+            //FCFS
+            //SJF
+            //Priority First
+            //Round Robin
+
+            Console.ReadKey();
+        }
+    }
+}
